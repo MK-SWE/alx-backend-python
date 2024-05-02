16ed37f40310403848eaca36ba9c277121b2bb66
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-02 16:32:10 -0700

    return a finction

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+make_multiplier that takes a float multiplier as argument and
+returns a function that multiplies a float by multiplier
+"""
+from typing import Callable, Iterator, Union, Optional, List, Tuple
+
+
+def make_multiplier(multiplier: float) -> float:
+    """
+    return multiplier a function
+    """
+    def mult(n: float) -> float:
+        """ multiplies a float by multiplier """
+        return float(n * multiplier)
+    return mult
