134b7ad2314dbfc6355a6016caf0f16b97a5eb3c
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-02 09:47:09 -0700

    sum list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+  sum_list  a function that return the sum of a given list
+"""
+
+
+def sum_list(list: list[float]) -> float:
+    """ Return the list sum"""
+    res = 0
+    for i in list:
+        res += i
+    return res
