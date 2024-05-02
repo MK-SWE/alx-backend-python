f0e12dc435fc05b0009ded999b9cf78d7f98dd89
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-02 07:42:36 -0700

    math.floor with types

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+Floor takes a float and return the result of math.floor()
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    return the floor of n
+    """
+    return math.floor(n)
