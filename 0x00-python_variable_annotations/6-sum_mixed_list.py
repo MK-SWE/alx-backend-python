07641fa826e08ec8726bce1d3806367e3890a151
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-02 15:16:17 -0700

    mixed datatypes

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+  return the sum of mixed datatype
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """
+    return the sum as float
+    """
+    return sum(mxd_lst)
