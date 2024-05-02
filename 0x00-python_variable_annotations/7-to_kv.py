8a50013b48fda9274e56d46c5f28f7ecea811508
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-02 16:00:09 -0700

    complex types

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""
+  take a value and return tuple
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """return a tuple"""
+    return (k, v**2)
