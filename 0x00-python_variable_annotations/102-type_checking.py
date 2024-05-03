ebe35a6e421fb97e23f4c9ff72caa659c7535352
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-03 12:08:19 -0700

    Duck typing

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""
+  Duck typing - first element of a sequence
+"""
+from typing import List, Tuple, Any
+
+
+def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List[Any]:
+    """ Return a zoomed array"""
+    zoomed_in: List[Any] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(tuple(array))
+
+zoom_3x = zoom_array(tuple(array), 3)
