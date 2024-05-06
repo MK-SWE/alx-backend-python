309aca58c9ce13c8d548f6ca744334eae3b8bf62
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-06 15:31:42 -0700

    task_wait_n

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""
+  Execute multiple coroutines in the same time
+"""
+import asyncio
+from typing import List
+task_wait_random  = __import__('3-tasks').task_wait_random 
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Create the async functions and return list of float"""
+    res = []
+    lst = []
+    for i in range(0, n):
+        task = task_wait_random(max_delay)
+        lst.append(task)
+    for tasks in asyncio.as_completed((lst)):
+        delay = await tasks
+        res.append(delay)
+    return res
