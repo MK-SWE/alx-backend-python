73d002c83b6a717e0fed894c9d91461281b66387
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-06 13:33:58 -0700

    test coroutine

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,34 +7,14 @@ import asyncio
 from typing import List
 
 
-
-def merge_sort(lst):
-    if len(lst) <= 1:
-        return lst
-    mid = len(lst) // 2
-    left_half = lst[:mid]
-    right_half = lst[mid:]
-    return merge(merge_sort(left_half), merge_sort(right_half))
-
-def merge(left, right):
-    merged = []
-    left_index = 0
-    right_index = 0
-    while left_index < len(left) and right_index < len(right):
-        if left[left_index] <= right[right_index]:
-            merged.append(left[left_index])
-            left_index += 1
-        else:
-            merged.append(right[right_index])
-            right_index += 1
-    merged.extend(left[left_index:])
-    merged.extend(right[right_index:])
-    return merged
-
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Create the async functions and return list of float"""
+    res = []
     lst = []
     for i in range(0, n):
-        delay = await wait_random(max_delay)
-        lst.append(delay)
-    res = merge_sort(lst)
+        task = wait_random(max_delay)
+        lst.append(task)
+    for tasks in asyncio.as_completed((lst)):
+        delay = await tasks
+        res.append(delay)
     return res
